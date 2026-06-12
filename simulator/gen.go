f8dcbae4a23d588f32b90435c1f9fb7d5be9b0ef
dario facchinetti <dario.fad@gmail.com>
2026-06-12 15:45:19 +0900

    Disable missing-declaration warnings for the genrated vmlinux.h

--- simulator/gen.go
+++ simulator/gen.go
@@ -1,3 +1,3 @@
 package simulator
 
-//go:generate go run github.com/cilium/ebpf/cmd/bpf2go -tags linux probe probe.c -- -DBENCH -O2 -g -Wall
+//go:generate go run github.com/cilium/ebpf/cmd/bpf2go -tags linux probe probe.c -- -DBENCH -O2 -g -Wall -Wno-missing-declarations
