9580310b67565cf87d2a2c9c06b8ceb9df253b58
dario facchinetti <dario.fad@gmail.com>
2026-06-24 11:17:44 +0900

    Format python files

--- client/demos_config.py
+++ client/demos_config.py
@@ -1,52 +1,61 @@
-import numpy as np
 import random
 
+import numpy as np
+
+
 def monit_M1_C1_trajectory() -> dict:
-    drel = np.array([float(i)/1000 for i in range(800)], dtype=np.float64)
+    drel = np.array([float(i) / 1000 for i in range(800)], dtype=np.float64)
     trajectory = dict()
     trajectory["DREL"] = drel.tolist()
     return trajectory
+
 
 def monit_M2_C2_trajectory() -> dict:
     trajectory = dict()
     return trajectory
 
+
 def monit_M3_C1_trajectory() -> dict:
-    pangle = np.array([float(i)/100000 for i in range(1001)], dtype=np.float64)
+    pangle = np.array([float(i) / 100000 for i in range(1001)], dtype=np.float64)
     trajectory = dict()
     trajectory["PANGLE"] = pangle.tolist()
     return trajectory
+
 
 def fals_M1_C1_trajectory() -> dict:
-    drel = np.array([float(i)/1000 for i in range(800)], dtype=np.float64)
+    drel = np.array([float(i) / 1000 for i in range(800)], dtype=np.float64)
     trajectory = dict()
     trajectory["DREL"] = drel.tolist()
     return trajectory
+
 
 def fals_M2_C1_trajectory() -> dict:
-    x = np.array([float(i)*0.01 for i in range(20)], dtype=np.float64)
-    y = np.array([float(i)*0.1 for i in range(20)], dtype=np.float64)    
+    x = np.array([float(i) * 0.01 for i in range(20)], dtype=np.float64)
+    y = np.array([float(i) * 0.1 for i in range(20)], dtype=np.float64)
     trajectory = dict()
     trajectory["X"] = x.tolist()
-    trajectory["Y"] = y.tolist()    
+    trajectory["Y"] = y.tolist()
     return trajectory
+
 
 def fals_M3_C2_trajectory() -> dict:
-    pangle = np.array([float(i)/100000 for i in range(1001)], dtype=np.float64)
-    rpm = np.array([float(i)/100000 for i in range(1001)], dtype=np.float64)    
+    pangle = np.array([float(i) / 100000 for i in range(1001)], dtype=np.float64)
+    rpm = np.array([float(i) / 100000 for i in range(1001)], dtype=np.float64)
     trajectory = dict()
     trajectory["PANGLE"] = pangle.tolist()
-    trajectory["RPM"] = rpm.tolist()    
+    trajectory["RPM"] = rpm.tolist()
     return trajectory
 
-def sign_M1_C2_trajectory(CYCLES=0) -> dict:
-    drel = np.array([float(i)/1000 for i in range(CYCLES)], dtype=np.float64)
+
+def sign_M1_C2_trajectory(CYCLES: int = 0) -> dict:
+    drel = np.array([float(i) / 1000 for i in range(CYCLES)], dtype=np.float64)
     trajectory = dict()
     trajectory["DREL"] = drel.tolist()
     return trajectory
 
-def sign_M1_C2_perturbation(PERIOD=0, ITERNO=0) -> dict:
-    drel = np.array([100.0], dtype=np.float64)    
+
+def sign_M1_C2_perturbation(PERIOD: int = 0, ITERNO: int = 0) -> dict:
+    drel = np.array([100.0], dtype=np.float64)
     PERIOD_START = 800
     time_trace = [PERIOD_START]
     perturbation = dict()
@@ -54,7 +63,8 @@ def sign_M1_C2_perturbation(PERIOD=0, ITERNO=0) -> dict:
     perturbation["time"] = np.array(time_trace, dtype=np.int32).tolist()
     return perturbation
 
-def sign_M2_C1_trajectory(CYCLES=0) -> dict:
+
+def sign_M2_C1_trajectory(CYCLES: int = 0) -> dict:
     X = np.array([10 + 0.0001 * (i + 1) for i in range(CYCLES)], dtype=np.float64)
     Y = np.array([20 for _ in range(CYCLES)], dtype=np.float64)
     trajectory = dict()
@@ -62,7 +72,8 @@ def sign_M2_C1_trajectory(CYCLES=0) -> dict:
     trajectory["Y"] = Y.tolist()
     return trajectory
 
-def sign_M2_C1_perturbation(PERIOD=0, ITERNO=0) -> dict:
+
+def sign_M2_C1_perturbation(PERIOD: int = 0, ITERNO: int = 0) -> dict:
     X = np.array([0.001 * (i + 1) for i in range(PERIOD)], dtype=np.float64)
     Y = np.array([0.02 for _ in range(PERIOD)], dtype=np.float64)
     PERIOD_START = 0 if ITERNO == 0 else PERIOD + random.randint(0, PERIOD // 2)
@@ -73,15 +84,17 @@ def sign_M2_C1_perturbation(PERIOD=0, ITERNO=0) -> dict:
     perturbation["time"] = np.array(time_trace, dtype=np.int32).tolist()
     return perturbation
 
-def sign_M3_C2_trajectory(CYCLES=0) -> dict:
-    pangle = np.array([float(i)/100000 for i in range(CYCLES)], dtype=np.float64)
-    rpm = np.array([float(i)/100000 for i in range(CYCLES)], dtype=np.float64)    
+
+def sign_M3_C2_trajectory(CYCLES: int = 0) -> dict:
+    pangle = np.array([float(i) / 100000 for i in range(CYCLES)], dtype=np.float64)
+    rpm = np.array([float(i) / 100000 for i in range(CYCLES)], dtype=np.float64)
     trajectory = dict()
     trajectory["PANGLE"] = pangle.tolist()
-    trajectory["RPM"] = rpm.tolist()    
+    trajectory["RPM"] = rpm.tolist()
     return trajectory
 
-def sign_M3_C2_perturbation(PERIOD=0, ITERNO=0) -> dict:
+
+def sign_M3_C2_perturbation(PERIOD: int = 0, ITERNO: int = 0) -> dict:
     pangle = np.array([-float(i) for i in range(10)], dtype=np.float64)
     PERIOD_START = 990
     time_trace = [PERIOD_START + i for i in range(10)]
@@ -91,7 +104,7 @@ def sign_M3_C2_perturbation(PERIOD=0, ITERNO=0) -> dict:
     return perturbation
 
 
-def state_M2_C1_trajectory(CYCLES=0) -> dict:
+def state_M2_C1_trajectory(CYCLES: int = 0) -> dict:
     X = np.array([10 + 0.0001 * (i + 1) for i in range(CYCLES)], dtype=np.float64)
     Y = np.array([20 for _ in range(CYCLES)], dtype=np.float64)
     trajectory = dict()
@@ -99,10 +112,11 @@ def state_M2_C1_trajectory(CYCLES=0) -> dict:
     trajectory["Y"] = Y.tolist()
     return trajectory
 
-def state_M2_C1_perturbation(PERIOD=0, ITERNO=0) -> list:
+
+def state_M2_C1_perturbation(PERIOD: int = 0, ITERNO: int = 0) -> list:
     TIME = np.uint32(5).item()
     VALUE_SIZE = np.uint32(8).item()
-    ADDR = np.uint64(0xaaaaaaac0018).item()
+    ADDR = np.uint64(0xAAAAAAAC0018).item()
     VALUE = np.uint64(50).item()
     perturbation = dict()
     perturbation["TIME"] = TIME
@@ -111,66 +125,71 @@ def state_M2_C1_perturbation(PERIOD=0, ITERNO=0) -> list:
     perturbation["VALUE"] = VALUE
     return [perturbation]
 
-def state_M3_C3_trajectory(CYCLES=0) -> dict:
+
+def state_M3_C3_trajectory(CYCLES: int = 0) -> dict:
     pangle = np.array([0.0 for i in range(CYCLES)], dtype=np.float64)
-    rpm = np.array([0.0 for i in range(CYCLES)], dtype=np.float64)    
+    rpm = np.array([0.0 for i in range(CYCLES)], dtype=np.float64)
     trajectory = dict()
     trajectory["PANGLE"] = pangle.tolist()
-    trajectory["RPM"] = rpm.tolist()    
+    trajectory["RPM"] = rpm.tolist()
     return trajectory
 
-def state_M3_C3_perturbation(PERIOD=0, ITERNO=0) -> list | None:
+
+def state_M3_C3_perturbation(PERIOD: int = 0, ITERNO: int = 0) -> list | None:
     TIME = np.uint32(50).item()
     VALUE_SIZE = np.uint32(8).item()
-    ADDR = np.uint64(0xaaaaaaad0278).item()
+    ADDR = np.uint64(0xAAAAAAAD0278).item()
     VALUE = np.float64(16).item()
     perturbation = dict()
     perturbation["TIME"] = TIME
     perturbation["VALUE_SIZE"] = VALUE_SIZE
     perturbation["ADDR"] = ADDR
     perturbation["VALUE"] = VALUE
-#    return None    
+    #    return None
     return [perturbation]
 
-def state_M1_C3_trajectory(CYCLES=0) -> dict:
+
+def state_M1_C3_trajectory(CYCLES: int = 0) -> dict:
     drel = np.array([0 for i in range(451)], dtype=np.float64)
     trajectory = dict()
     trajectory["DREL"] = drel.tolist()
     return trajectory
 
-def state_M1_C3_perturbation(PERIOD=0, ITERNO=0) -> list:
+
+def state_M1_C3_perturbation(PERIOD: int = 0, ITERNO: int = 0) -> list:
     VALUE_SIZE = np.uint32(8).item()
-    ADDR = np.uint64(0x55555556e350).item()
+    ADDR = np.uint64(0x55555556E350).item()
     perturbations = []
     for i in range(30):
-        TIME = np.uint32(300+i).item()
+        TIME = np.uint32(300 + i).item()
         VALUE = np.float64(25).item()
-#        VALUE = np.float64(75).item()                
+        #        VALUE = np.float64(75).item()
         perturbation = dict()
         perturbation["TIME"] = TIME
         perturbation["VALUE_SIZE"] = VALUE_SIZE
         perturbation["ADDR"] = ADDR
         perturbation["VALUE"] = VALUE
         perturbations.append(perturbation)
-#    return perturbations        
-    return []        
+    #    return perturbations
+    return []
 
 
-def sign_M3_C4_trajectory(CYCLES=0) -> dict:
+def sign_M3_C4_trajectory(CYCLES: int = 0) -> dict:
     pangle = np.array([0.0 for _ in range(CYCLES)], dtype=np.float64)
     trajectory = dict()
     trajectory["PANGLE"] = pangle.tolist()
     return trajectory
 
-def sign_M3_C4_perturbation(PERIOD=0, ITERNO=0) -> dict | None:
-    l = [float(i)/30 for i in range(450)]
-    pangle = np.array(l, dtype=np.float64)
+
+def sign_M3_C4_perturbation(PERIOD: int = 0, ITERNO: int = 0) -> dict | None:
+    angles = [float(i) / 30 for i in range(450)]
+    pangle = np.array(angles, dtype=np.float64)
     PERIOD_START = 50
     time_trace = [PERIOD_START + i for i in range(450)]
     perturbation = dict()
     perturbation["PANGLE"] = pangle.tolist()
     perturbation["time"] = np.array(time_trace, dtype=np.int32).tolist()
     return perturbation
-#return None
 
 
+# return None
--- client/plots/accdata.py
+++ client/plots/accdata.py
@@ -1,11 +1,14 @@
-import redis
 import csv
 import sys
+from collections.abc import Awaitable
 
-REDIS_HOST = 'localhost'
+import redis
+from redis.exceptions import ConnectionError as RedisConnectionError
+
+REDIS_HOST = "localhost"
 REDIS_PORT = 6379
 REDIS_DB = 0
-ZSET_KEY = 'simulation:0'
+ZSET_KEY = "simulation:0"
 
 signals = {
     "TIME": 0,
@@ -14,41 +17,55 @@ signals = {
     "VEGO": 3,
     "VREL": 4,
     "DLEAD": 5,
-    "APOS": 6,    
+    "APOS": 6,
 }
 
-if len(sys.argv) > 1:
+
+def main() -> None:
+    if len(sys.argv) <= 1:
+        raise SystemExit("You must provide the name of the csv file to save the data")
+
     fname = sys.argv[1]
     print(f"fname: {fname}")
-else:
-    print("You must provide the name of the csv file to save the data")
 
-with open(fname, mode='w+', newline='') as file:
-    writer = csv.writer(file)
-    try:
-        r = redis.Redis(host=REDIS_HOST, port=REDIS_PORT, db=REDIS_DB, decode_responses=True)
-        print(f"Connected to Redis at {REDIS_HOST}:{REDIS_PORT}")
-        all_records = r.zrange(ZSET_KEY, 0, -1, withscores=True)
-        trajectory = []
-        phi = []
-        ow_drel = []
-        real_drel = []
-        i = 0
-        writer.writerow(["cycle", "real_drel", "ow_drel", "phi"])
-        for member, _ in all_records:
-            record = list(map(float, member.split(",")))
-            trajectory.append(record)
-            ow_drel.append(record[signals["DREL"]])                
-            real_drel.append(record[signals["DLEAD"]] - (record[signals["APOS"]] +10))
-            phi.append(real_drel[-1] - 1.4 * record[signals["VEGO"]])
-            writer.writerow([i, real_drel[i], ow_drel[i], phi[i]])
-            i += 1
-            
-    except redis.exceptions.ConnectionError as e:
-        print(f"Error connecting to Redis: {e}")
-    except Exception as e:
-        print(f"An unexpected error occurred: {e}")
-    finally:
-        if 'r' in locals():
-            r.close()
-            print("\nRedis connection closed.")
+    r: redis.Redis | None = None
+    with open(fname, mode="w+", newline="") as file:
+        writer = csv.writer(file)
+        try:
+            r = redis.Redis(
+                host=REDIS_HOST, port=REDIS_PORT, db=REDIS_DB, decode_responses=True
+            )
+            print(f"Connected to Redis at {REDIS_HOST}:{REDIS_PORT}")
+            all_records = r.zrange(ZSET_KEY, 0, -1, withscores=True)
+            if isinstance(all_records, Awaitable):
+                raise TypeError("redis.zrange returned an awaitable response")
+            all_records = list(all_records)
+            trajectory = []
+            phi = []
+            ow_drel = []
+            real_drel = []
+            i = 0
+            writer.writerow(["cycle", "real_drel", "ow_drel", "phi"])
+            for member, _ in all_records:
+                record = list(map(float, member.split(",")))
+                trajectory.append(record)
+                ow_drel.append(record[signals["DREL"]])
+                real_drel.append(
+                    record[signals["DLEAD"]] - (record[signals["APOS"]] + 10)
+                )
+                phi.append(real_drel[-1] - 1.4 * record[signals["VEGO"]])
+                writer.writerow([i, real_drel[i], ow_drel[i], phi[i]])
+                i += 1
+
+        except RedisConnectionError as e:
+            print(f"Error connecting to Redis: {e}")
+        except Exception as e:
+            print(f"An unexpected error occurred: {e}")
+        finally:
+            if r is not None:
+                r.close()
+                print("\nRedis connection closed.")
+
+
+if __name__ == "__main__":
+    main()
